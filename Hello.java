53f5f54a00b92bdbccba8de28aeb04c205bf4674
ncgeh <112000718+ncgeh@users.noreply.github.com>
2022-08-24 11:35:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*******************************
+* Hello.java
+********************************
+* Neal Gehring
+* 08/24/2022
+*******************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Neal Gehring");
+		System.out.println("Hello.java");
+		System.out.println("08/24/2022");
+		System.out.println("CMSC255 Lab01");
+		System.out.println();
+		
+		}
+	}
